4447bcff4de2a0079b9fb64d93caf8b559d68591
Daniel Thul <daniel.thul@gmail.com>
2019-07-19 12:04:31 +0200

    Use mentions to talk to the bot. Also supports direct messages.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,71 @@
-use lazy_static::lazy_static;
 use redis::Commands;
 use regex::Regex;
 use serenity::{
     model::{
-        channel::Message, channel::PermissionOverwrite, channel::PermissionOverwriteType,
-        gateway::Ready, id::RoleId, id::UserId, permissions::Permissions,
+        channel::Channel, channel::Message, channel::PermissionOverwrite,
+        channel::PermissionOverwriteType, gateway::Ready, id::RoleId, id::UserId,
+        permissions::Permissions,
     },
     prelude::*,
 };
 use std::env;
 use std::sync::Arc;
 
-lazy_static! {
-    static ref CREATE_CHANNEL_REGEX: Regex =
-        Regex::new(r"^!createchannel\s+(?P<channelname>(?:[0-9a-zA-Z_]+\-?)+)\s*$").unwrap();
-    static ref SETMYMEETUPID_REGEX: Regex =
-        Regex::new(r"^!setmeetupid\s+(?P<meetupid>[0-9]+)\s*$").unwrap();
+const CHANNEL_NAME_PATTERN: &'static str = r"(?:[0-9a-zA-Z_]+\-?)+"; // TODO: this is too strict
+const MENTION_PATTERN: &'static str = r"<@[0-9]+>";
+
+struct Regexes {
+    bot_mention: String,
+    create_channel_dm: Regex,
+    create_channel_mention: Regex,
+    link_meetup_dm: Regex,
+    link_meetup_mention: Regex,
+}
+
+impl Regexes {
+    fn create_channel(&self, is_dm: bool) -> &Regex {
+        if is_dm {
+            &self.create_channel_dm
+        } else {
+            &self.create_channel_mention
+        }
+    }
+
+    fn link_meetup(&self, is_dm: bool) -> &Regex {
+        if is_dm {
+            &self.link_meetup_dm
+        } else {
+            &self.link_meetup_mention
+        }
+    }
+}
+
+fn compile_regexes(bot_id: u64) -> Regexes {
+    let bot_mention = format!(r"<@{}>", bot_id);
+    let create_channel = format!(
+        r"create[ -]?channel\s+(?P<channelname>{cnp})",
+        cnp = CHANNEL_NAME_PATTERN
+    );
+    let create_channel_dm = format!(r"^{create_channel}\s*$", create_channel = create_channel);
+    let create_channel_mention = format!(
+        r"^{bot_mention}\s+{create_channel}\s*$",
+        bot_mention = bot_mention,
+        create_channel = create_channel
+    );
+    let link_meetup = r"link[ -]?meetup\s+(?P<meetupid>[0-9]+)";
+    let link_meetup_dm = format!(r"^{link_meetup}\s*$", link_meetup = link_meetup);
+    let link_meetup_mention = format!(
+        r"^{bot_mention}\s+{link_meetup}\s*$",
+        bot_mention = bot_mention,
+        link_meetup = link_meetup
+    );
+    Regexes {
+        bot_mention: bot_mention,
+        create_channel_dm: Regex::new(create_channel_dm.as_str()).unwrap(),
+        create_channel_mention: Regex::new(create_channel_mention.as_str()).unwrap(),
+        link_meetup_dm: Regex::new(link_meetup_dm.as_str()).unwrap(),
+        link_meetup_mention: Regex::new(link_meetup_mention.as_str()).unwrap(),
+    }
 }
 
 struct BotIdKey;
@@ -28,6 +78,11 @@ impl TypeMapKey for RedisConnectionKey {
     type Value = Arc<Mutex<redis::Connection>>;
 }
 
+struct RegexesKey;
+impl TypeMapKey for RegexesKey {
+    type Value = Arc<Regexes>;
+}
+
 struct Handler;
 
 impl EventHandler for Handler {
@@ -37,108 +92,116 @@ impl EventHandler for Handler {
     // Event handlers are dispatched through a threadpool, and so multiple
     // events can be dispatched simultaneously.
     fn message(&self, ctx: Context, msg: Message) {
-        if !msg.content.starts_with("!") {
-            // Quickly bail before any more expensive tests
+        let channel = match msg.channel_id.to_channel(&ctx) {
+            Ok(channel) => channel,
+            _ => return,
+        };
+        // Is this a direct message to the bot?
+        let mut is_dm = match channel {
+            Channel::Private(_) => true,
+            _ => false,
+        };
+        let (bot_id, regexes) = {
+            let data = ctx.data.read();
+            let regexes = data
+                .get::<RegexesKey>()
+                .expect("Regexes were not compiled")
+                .clone();
+            let bot_id = data.get::<BotIdKey>().expect("Bot ID was not set").clone();
+            (bot_id, regexes)
+        };
+        println!("{}", msg.content);
+        // Ignore all messages written by the bot itself
+        if msg.author.id == bot_id {
             return;
         }
-        if msg.content == "!help" {
-            let _ = msg.channel_id.say(
-                &ctx.http,
-                "Available commands:\n!help\n!ping\n!createchannel",
-            );
-        } else if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
-                println!("Error sending message: {:?}", why);
-            }
-        } else if msg.content.starts_with("!setmeetupid") {
-            if let Some(captures) = SETMYMEETUPID_REGEX.captures(&msg.content) {
-                let user_id = msg.author.id.0;
-                let meetup_id = captures.name("meetupid").unwrap().as_str();
-                let redis_key = format!("user:{}:meetupid", user_id);
-                let redis_connection_mutex = {
-                    ctx.data
-                        .read()
-                        .get::<RedisConnectionKey>()
-                        .expect("Redis connection was not set")
-                        .clone()
-                };
-                {
-                    let mut redis_connection = redis_connection_mutex.lock();
-                    if let Ok(()) = redis_connection.sadd("users", user_id) {
-                        if let Ok(()) = redis_connection.set(&redis_key, meetup_id) {
-                            let _ = msg.channel_id.say(&ctx.http, format!("Assigned meetup id"));
-                            return;
-                        }
+        // If the message is not a direct message and does not start with a
+        // mention of the bot, ignore it
+        if !is_dm && !msg.content.starts_with(&regexes.bot_mention) {
+            let _ = msg.channel_id.say(&ctx.http, "Message ignored");
+            return;
+        }
+        // If the message is a direct message but starts with a mention of the bot,
+        // switch to the non-DM parsing
+        if is_dm && msg.content.starts_with(&regexes.bot_mention) {
+            is_dm = false;
+        }
+        // TODO: might want to use a RegexSet here to speed up matching
+        if let Some(captures) = regexes.link_meetup(is_dm).captures(&msg.content) {
+            let user_id = msg.author.id.0;
+            let meetup_id = captures.name("meetupid").unwrap().as_str();
+            let redis_key = format!("user:{}:meetupid", user_id);
+            let redis_connection_mutex = {
+                ctx.data
+                    .read()
+                    .get::<RedisConnectionKey>()
+                    .expect("Redis connection was not set")
+                    .clone()
+            };
+            {
+                let mut redis_connection = redis_connection_mutex.lock();
+                if let Ok(()) = redis_connection.sadd("users", user_id) {
+                    if let Ok(()) = redis_connection.set(&redis_key, meetup_id) {
+                        let _ = msg.channel_id.say(&ctx.http, format!("Assigned meetup id"));
+                        return;
                     }
-                    let _ = msg
-                        .channel_id
-                        .say(&ctx.http, "Could not assign meetup id (internal error)");
                 }
+                let _ = msg
+                    .channel_id
+                    .say(&ctx.http, "Could not assign meetup id (internal error)");
             }
-        } else if msg.content.starts_with("!createchannel") {
+        } else if let Some(captures) = regexes.create_channel(is_dm).captures(&msg.content) {
             // TODO: check permission
-            if let Some(captures) = CREATE_CHANNEL_REGEX.captures(&msg.content) {
-                let channel_name = captures.name("channelname").unwrap().as_str();
-                if let Some(guild_id) = msg.guild_id {
-                    match guild_id.create_role(&ctx, |role_builder| {
-                        role_builder
-                            .name(channel_name)
-                            .permissions(Permissions::empty())
-                    }) {
-                        Ok(role_channel) => {
-                            // Make sure that the user that issued this command is assigned the new role
-                            let _ = ctx.http.add_member_role(
-                                guild_id.0,
-                                msg.author.id.0,
-                                role_channel.id.0,
-                            );
-                            // The @everyone role has the same id as the guild
-                            let role_everyone_id = RoleId(guild_id.0);
-                            // The bot's user id is stored in the context
-                            let bot_id = {
-                                *ctx.data
-                                    .read()
-                                    .get::<BotIdKey>()
-                                    .expect("Bot ID was not set")
-                            };
-                            // Make this channel private.
-                            // This is achieved by denying @everyone the READ_MESSAGES permission
-                            // but allowing the now role the READ_MESSAGES permission.
-                            // see: https://support.discordapp.com/hc/en-us/articles/206143877-How-do-I-set-up-a-Role-Exclusive-channel-
-                            let permission_overwrites = vec![
-                                PermissionOverwrite {
-                                    allow: Permissions::empty(),
-                                    deny: Permissions::READ_MESSAGES,
-                                    kind: PermissionOverwriteType::Role(role_everyone_id),
-                                },
-                                PermissionOverwrite {
-                                    allow: Permissions::READ_MESSAGES
-                                        | Permissions::MENTION_EVERYONE,
-                                    deny: Permissions::empty(),
-                                    kind: PermissionOverwriteType::Role(role_channel.id),
-                                },
-                                PermissionOverwrite {
-                                    allow: Permissions::READ_MESSAGES,
-                                    deny: Permissions::empty(),
-                                    kind: PermissionOverwriteType::Member(bot_id),
-                                },
-                            ];
-                            let _ = guild_id.create_channel(&ctx, |channel_builder| {
-                                channel_builder
-                                    .name(channel_name)
-                                    .permissions(permission_overwrites)
-                            });
-                        }
-                        _ => {}
-                    };
-                }
-            } else {
-                let _ = msg.channel_id.say(&ctx.http, "Did not create channel. This is how to use the command:\n!createchannel channel_name");
+            let channel_name = captures.name("channelname").unwrap().as_str();
+            if let Some(guild_id) = msg.guild_id {
+                match guild_id.create_role(&ctx, |role_builder| {
+                    role_builder
+                        .name(channel_name)
+                        .permissions(Permissions::empty())
+                }) {
+                    Ok(role_channel) => {
+                        // Make sure that the user that issued this command is assigned the new role
+                        let _ = ctx.http.add_member_role(
+                            guild_id.0,
+                            msg.author.id.0,
+                            role_channel.id.0,
+                        );
+                        // The @everyone role has the same id as the guild
+                        let role_everyone_id = RoleId(guild_id.0);
+                        // Make this channel private.
+                        // This is achieved by denying @everyone the READ_MESSAGES permission
+                        // but allowing the now role the READ_MESSAGES permission.
+                        // see: https://support.discordapp.com/hc/en-us/articles/206143877-How-do-I-set-up-a-Role-Exclusive-channel-
+                        let permission_overwrites = vec![
+                            PermissionOverwrite {
+                                allow: Permissions::empty(),
+                                deny: Permissions::READ_MESSAGES,
+                                kind: PermissionOverwriteType::Role(role_everyone_id),
+                            },
+                            PermissionOverwrite {
+                                allow: Permissions::READ_MESSAGES | Permissions::MENTION_EVERYONE,
+                                deny: Permissions::empty(),
+                                kind: PermissionOverwriteType::Role(role_channel.id),
+                            },
+                            PermissionOverwrite {
+                                allow: Permissions::READ_MESSAGES,
+                                deny: Permissions::empty(),
+                                kind: PermissionOverwriteType::Member(bot_id),
+                            },
+                        ];
+                        let _ = guild_id.create_channel(&ctx, |channel_builder| {
+                            channel_builder
+                                .name(channel_name)
+                                .permissions(permission_overwrites)
+                        });
+                    }
+                    _ => {}
+                };
             }
+        } else {
+            let _ = msg
+                .channel_id
+                .say(&ctx.http, "Sorry, I do not understand that command");
         }
     }
 
@@ -175,11 +238,15 @@ fn main() {
         Err(why) => panic!("Could not access application info: {:?}", why),
     };
 
+    // pre-compile the regexes
+    let regexes = compile_regexes(bot_id.0);
+
     // Store the bot's id in the client for easy access
     {
         let mut data = client.data.write();
         data.insert::<BotIdKey>(bot_id);
         data.insert::<RedisConnectionKey>(Arc::new(Mutex::new(connection)));
+        data.insert::<RegexesKey>(Arc::new(regexes));
     }
 
     // Finally, start a single shard, and start listening to events.
