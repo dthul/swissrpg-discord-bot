c28763ec4e30ad7360ae771419b566f2869fb412
Daniel Thul <daniel.thul@gmail.com>
2019-12-11 17:02:16 +0100

    Add a release compile script for Windows.

---
+++ docker/compile.bat
@@ -0,0 +1,5 @@
+:: Start the container
+docker start swissrpg-compile-container
+:: Run the build command inside of the container
+:: TODO: replace 'cp /tmp/target/release...' with Cargo's --out-dir option once it is stable
+docker exec -it swissrpg-compile-container /bin/bash -lc "cd /opt/app && cargo build --release --target-dir /tmp/target && cp /tmp/target/release/swissrpg-app /opt/"
