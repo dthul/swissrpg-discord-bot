ecb1639f9ed6435f93ac0304b38d4ec63357d0ab
Daniel Thul <daniel.thul@gmail.com>
2020-01-31 15:53:56 +0100

    Make instantaneous subscription role assignment actually work.

--- ui/src/web/stripe_webhook_endpoint.rs
+++ ui/src/web/stripe_webhook_endpoint.rs
@@ -17,7 +17,7 @@ pub fn create_routes(
                 let stripe_webhook_secret = stripe_webhook_secret.clone();
                 let discord_cache_http = discord_cache_http.clone();
                 let stripe_client = stripe_client.clone();
-                async move {
+                let webhook_handler_future = async move {
                     if let Ok(payload) = std::str::from_utf8(&payload) {
                         if let Ok(event) = stripe::Webhook::construct_event(
                             &payload,
@@ -49,8 +49,9 @@ pub fn create_routes(
                     } else {
                         eprintln!("Payload to UTF8 conversion failed");
                     }
-                    Ok::<_, warp::Rejection>("Webhook!")
-                }
+                };
+                tokio::spawn(webhook_handler_future);
+                async { Ok::<_, warp::Rejection>("Webhook!") }
             })
     };
     get_route
@@ -71,6 +72,14 @@ async fn handle_new_subscription(
     let (customer, product) =
         lib::tasks::subscription_roles::get_customer_and_product(stripe_client, subscription)
             .await?;
+    println!(
+        "Received new subscription with product '{:#?}'",
+        product.name
+    );
+    // Wait for a few seconds and re-query the customer object from Stripe,
+    // since the webhook is sometimes faster than the customer metadata is updated
+    tokio::time::delay_for(tokio::time::Duration::from_secs(20)).await;
+    let customer = stripe::Customer::retrieve(stripe_client, &customer.id, &[]).await?;
     if let Some(username) = customer.metadata.get("Discord") {
         // Try to find the Discord user associated with this subscription
         let ids = lib::tasks::subscription_roles::discord_usernames_to_ids(
@@ -85,6 +94,7 @@ async fn handle_new_subscription(
                 lib::tasks::subscription_roles::INSIDER_PRODUCT_REGEX.is_match(name)
             });
             if is_champion_product {
+                println!("Adding Champion role");
                 lib::tasks::subscription_roles::add_member_role(
                     discord_api.clone(),
                     *discord_id,
@@ -93,6 +103,7 @@ async fn handle_new_subscription(
                 .await?;
             }
             if is_insider_product {
+                println!("Adding Insider role");
                 lib::tasks::subscription_roles::add_member_role(
                     discord_api.clone(),
                     *discord_id,
@@ -100,7 +111,14 @@ async fn handle_new_subscription(
                 )
                 .await?;
             }
+        } else {
+            eprintln!(
+                "Could not match the Discord username '{}' to an actual Discord user",
+                username
+            );
         }
-    };
+    } else {
+        eprintln!("Found no 'Discord' field on the customer metadata object");
+    }
     Ok("")
 }
