9cab677f8a251e46d948b27109b7da1ba97401d6
Daniel Thul <daniel.thul@gmail.com>
2020-03-08 14:40:45 +0100

    Add Redis locking functions.

--- lib/src/redis.rs
+++ lib/src/redis.rs
@@ -1,4 +1,5 @@
 use futures_util::FutureExt;
+use rand::Rng;
 use redis::{AsyncCommands, Commands};
 use std::future::Future;
 use std::pin::Pin;
@@ -127,4 +128,137 @@ pub fn meetup_to_discord_ids(
         .cloned()
         .zip(discord_user_ids?.into_iter())
         .collect())
+}
+
+pub struct Lock<'a> {
+    redis_connection: &'a mut redis::Connection,
+    lockname: &'a str,
+    identifier: u64,
+}
+
+// https://redislabs.com/ebook/part-2-core-concepts/chapter-6-application-components-in-redis/6-2-distributed-locking/6-2-5-locks-with-timeouts/
+impl<'a> Lock<'a> {
+    pub fn acquire_with_timeout(
+        redis_connection: &'a mut redis::Connection,
+        lockname: &'a str,
+        acquire_timeout: std::time::Duration,
+        lock_timeout: std::time::Duration,
+    ) -> Result<Option<Self>, crate::meetup::Error> {
+        let identifier: u64 = rand::thread_rng().gen();
+        let start = std::time::Instant::now();
+        while start.elapsed() < acquire_timeout {
+            let was_set: u8 = redis_connection.set_nx(lockname, identifier)?;
+            if was_set == 1 {
+                redis_connection.expire(lockname, lock_timeout.as_secs() as usize)?;
+                return Ok(Some(Lock {
+                    redis_connection,
+                    lockname,
+                    identifier,
+                }));
+            } else {
+                // We failed to get the lock.
+                // Make sure that a timeout is set
+                let current_timeout: isize = redis_connection.ttl(lockname)?;
+                if current_timeout < 0 {
+                    // No timeout set: set it now
+                    redis_connection.expire(lockname, lock_timeout.as_secs() as usize)?;
+                }
+            }
+            // Sleep for a moment and re-try
+            std::thread::sleep(std::time::Duration::from_millis(1));
+        }
+        Ok(None)
+    }
+
+    fn release(&mut self) -> Result<(), crate::meetup::Error> {
+        let lockname = self.lockname;
+        let identifier = self.identifier;
+        let _: () = redis::transaction(self.redis_connection, &[self.lockname], |con, pipe| {
+            let current_identifier: u64 = con.get(lockname)?;
+            if current_identifier == identifier {
+                pipe.del(lockname);
+            }
+            pipe.query(con)
+        })?;
+        Ok(())
+    }
+
+    pub fn con(&mut self) -> &mut redis::Connection {
+        self.redis_connection
+    }
+}
+
+impl<'a> Drop for Lock<'a> {
+    fn drop(&mut self) {
+        if let Err(err) = self.release() {
+            eprintln!("Error when trying to release Redis lock:\n{:#?}", err);
+        }
+    }
+}
+
+pub struct AsyncLock<'a> {
+    redis_connection: &'a mut redis::aio::Connection,
+    lockname: &'a str,
+    identifier: u64,
+}
+
+impl<'a> AsyncLock<'a> {
+    pub async fn acquire_with_timeout(
+        redis_connection: &'a mut redis::aio::Connection,
+        lockname: &'a str,
+        acquire_timeout: std::time::Duration,
+        lock_timeout: std::time::Duration,
+    ) -> Result<Option<AsyncLock<'a>>, crate::meetup::Error> {
+        let identifier: u64 = rand::thread_rng().gen();
+        let start = std::time::Instant::now();
+        while start.elapsed() < acquire_timeout {
+            let was_set: u8 = redis_connection.set_nx(lockname, identifier).await?;
+            if was_set == 1 {
+                redis_connection
+                    .expire(lockname, lock_timeout.as_secs() as usize)
+                    .await?;
+                return Ok(Some(AsyncLock {
+                    redis_connection,
+                    lockname,
+                    identifier,
+                }));
+            } else {
+                // We failed to get the lock.
+                // Make sure that a timeout is set
+                let current_timeout: isize = redis_connection.ttl(lockname).await?;
+                if current_timeout < 0 {
+                    // No timeout set: set it now
+                    redis_connection
+                        .expire(lockname, lock_timeout.as_secs() as usize)
+                        .await?;
+                }
+            }
+            // Sleep for a moment and re-try
+            std::thread::sleep(std::time::Duration::from_millis(1));
+        }
+        Ok(None)
+    }
+
+    // There is no AsyncDrop yet, so release is manual
+    pub async fn release(self) -> Result<(), crate::meetup::Error> {
+        let lockname = self.lockname;
+        let identifier = self.identifier;
+        let _: () = async_redis_transaction(self.redis_connection, &[lockname], |con, mut pipe| {
+            let lockname = lockname.to_string();
+            async move {
+                let current_identifier: u64 = con.get(&lockname).await?;
+                if current_identifier == identifier {
+                    pipe.del(&lockname);
+                }
+                pipe.query_async(con).await
+            }
+            .boxed()
+        })
+        .await?;
+        Ok(())
+    }
+
+    pub fn con(&mut self) -> &mut redis::aio::Connection {
+        self.redis_connection
+    }
 }
