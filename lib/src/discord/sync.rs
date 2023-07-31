75ab64333adbb78b362e82b44b99797f506ad546
Daniel Thul <daniel.thul@gmail.com>
2023-07-31 19:39:40 +0100

    Remove Rythm bot permission overwrites. This bot is not used anymore.

--- lib/src/discord/sync.rs
+++ lib/src/discord/sync.rs
@@ -20,7 +20,6 @@ pub mod ids {
     pub const ORGANISER_ID: RoleId = RoleId(689914933357314090);
     pub const GAME_MASTER_ID: RoleId = RoleId(606913167439822987);
     pub const DICE_ROLLER_BOT_ID: Option<RoleId> = None;
-    pub const RYTHM_BOT_IDS: &'static [RoleId] = &[RoleId(731075146911187005)];
     pub const ONE_SHOT_CATEGORY_IDS: &'static [ChannelId] = &[ChannelId(607561808429056042)];
     pub const CAMPAIGN_CATEGORY_IDS: &'static [ChannelId] = &[ChannelId(607561949651402772)];
     pub const VOICE_CHANNELS_CATEGORY_IDS: &'static [ChannelId] = &[ChannelId(601070848446824512)];
@@ -39,8 +38,6 @@ pub mod ids {
     pub const ORGANISER_ID: RoleId = RoleId(539447673988841492);
     pub const GAME_MASTER_ID: RoleId = RoleId(412946716892069888);
     pub const DICE_ROLLER_BOT_ID: Option<RoleId> = Some(RoleId(600612886368223274));
-    pub const RYTHM_BOT_IDS: &'static [RoleId] =
-        &[RoleId(731078776502353940), RoleId(731078935319937054)];
     pub const ONE_SHOT_CATEGORY_IDS: &'static [ChannelId] = &[ChannelId(562607292176924694)];
     pub const CAMPAIGN_CATEGORY_IDS: &'static [ChannelId] =
         &[ChannelId(414074722259828736), ChannelId(651006290998329354)];
@@ -862,13 +859,6 @@ async fn sync_channel_permissions(
                     kind: PermissionOverwriteType::Role(ORGANISER_ID),
                 },
             ];
-            for &rythm_bot_id in RYTHM_BOT_IDS {
-                permission_overwrites.push(PermissionOverwrite {
-                    allow: Permissions::VIEW_CHANNEL | Permissions::CONNECT,
-                    deny: Permissions::empty(),
-                    kind: PermissionOverwriteType::Role(rythm_bot_id),
-                });
-            }
             for &host_id in discord_host_ids {
                 permission_overwrites.push(PermissionOverwrite {
                     allow: Permissions::CONNECT
