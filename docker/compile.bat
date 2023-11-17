d2ee9ccc4e3011bef3cb30836094802ecb9b01c8
Daniel Thul <daniel.thul@gmail.com>
2023-11-17 20:19:54 +0000

    Remove unused Docker related files.

--- docker/compile.bat
+++
@@ -1,6 +0,0 @@
-:: docker create -it --volume C:\Users\dthul\repos\discord-bot:/opt --name=swissrpg-compile-container swissrpg-bot
-:: Start the container
-docker start swissrpg-compile-container
-:: Run the build command inside of the container
-:: TODO: replace 'cp /tmp/target/release...' with Cargo's --out-dir option once it is stable
-docker exec -it swissrpg-compile-container /bin/bash -lc "cd /opt/app && cargo build --release --target-dir /tmp/target && cp /tmp/target/release/swissrpg-app /opt/"
--- docker/compile_test_fast.bat
+++
@@ -1,5 +0,0 @@
-:: Start the container
-docker start swissrpg-compile-container
-:: Run the build command inside of the container
-:: TODO: replace 'cp /tmp/target/release...' with Cargo's --out-dir option once it is stable
-docker exec -it swissrpg-compile-container /bin/bash -lc "cd /opt/app && cargo build --features \"bottest\" --target-dir /tmp/target && cp /tmp/target/debug/swissrpg-app /opt/swissrpg-app-test"
